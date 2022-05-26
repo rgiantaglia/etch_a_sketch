fa5dea993ec0934c6d906cadba86abf55f4852a7
Rodolfo Giantaglia <rodolfo@sugarmapleinteractive.com>
2022-05-26 12:35:12 -0400

    fix the issue that when clicking on button new grid was being created under old grid. Adding container.replaceChildren(); before the function is called did the trick.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,50 @@
-const container = document.querySelector('#container');
 
 
-function grid(){
-
-const div = document.createElement('div');
-div.classList.add('row');
-container.appendChild(div);
-
-
-for (let j=1; j<=n; j++){
-const row = document.querySelector('.row');
-
-const divRow = document.createElement('div');
-divRow.classList.add('square');
-div.appendChild(divRow);
-};
-
-}
+const btn = document.querySelector('button')
 
 
 
+
+
+btn.addEventListener('click', function() {
+
+
+
+    const container = document.querySelector('#container');
+
+    container.replaceChildren();
+    n = prompt('How many squares per side do you want?');
+
+    function grid(){
+
+        for (let i=1; i <= n; i++){
+
+            const div = document.createElement('div');
+            div.classList.add('row');
+            container.appendChild(div);
+            
+
+            for (let j=1; j<=n; j++){
+                const row = document.querySelector('.row');
+
+                const divRow = document.createElement('div');
+                divRow.classList.add('square');
+                div.appendChild(divRow);
+                
+            };
+        }
+    }
+
+    grid();
+
+});
 
 const hover = document.querySelector('#container');
 
 hover.addEventListener('mouseover', function(event) {
 
-event.target.style.backgroundColor = 'red';
-document.getElementById('container').style.backgroundColor = 'transparent';
+    event.target.style.backgroundColor = 'red';
+    document.getElementById('container').style.backgroundColor = 'transparent';
 
 });
-
-
-
-const n = prompt('choose a number');
-for (let i=1; i <= n; i++){
-    grid ();
-}
 
